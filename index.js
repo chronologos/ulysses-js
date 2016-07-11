090b1d16fe9b946986f734da7dadeca0d7c9bd6d
yt61@duke.edu <yt61@duke.edu>
2016-07-11 05:31:16 -0700

    db 2

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-var url = process.env.MONGODB_URI;
+
+if (process.env.NODE_ENV === 'production') {
+    var url = process.env.MONGODB_URI;
+}
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server.");
