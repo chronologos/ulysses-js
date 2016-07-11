61e5b3fddecdf7761fcadd7969f36fe33712a511
yt61@duke.edu <yt61@duke.edu>
2016-07-11 07:46:29 -0700

    config

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
+var port = process.env.PORT || 3000;
 if (process.env.NODE_ENV === 'production') {
     var url = process.env.MONGODB_URI;
-    var PORT = 80;
 }
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
@@ -16,6 +16,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(PORT, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
