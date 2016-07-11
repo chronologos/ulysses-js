dc78fc23a323cb3eee1415d611c4a2e2155434bf
yt61@duke.edu <yt61@duke.edu>
2016-07-11 08:14:59 -0700

    createContract3

--- index.js
+++ index.js
@@ -14,19 +14,19 @@ app.get('/', function (req, res) {
 app.post('/contract', function (req, res) {
     res.format({
         'application/json': function(){
-            res.send({ 'userId': req.param('userId') })
-        },
+            res.send({ 'userId': req.params.userId}) }
     });
+});
 
-    MongoClient.connect(url, function(err, db) {
-        assert.equal(null, err);
-        console.log("Connected correctly to server.");
-        var contracts_db = db.collection('uly-dev');
-        contracts_db.insert(data, function(err,result) {
-            if(err) throw err;
-            console.log(result);
-        });
-    });
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server.");
+    var contracts_db = db.collection('uly-dev');
+    //contracts_db.insert(data, function(err,result) {
+    //if(err) throw err;
+    //console.log(result);
+    //});
+    db.close()
 });
 
 app.listen(port, function () {
