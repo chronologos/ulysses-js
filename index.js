2f351f0a7d577b54720981bc358e7e10bf49949b
yt61@duke.edu <yt61@duke.edu>
2016-07-11 04:44:12 -0700

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
+
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
