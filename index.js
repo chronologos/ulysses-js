a0a22b505247cb17971270806f36203364c3908f
yt61@duke.edu <yt61@duke.edu>
2016-07-11 05:36:22 -0700

    db 3

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ if (process.env.NODE_ENV === 'production') {
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server.");
-  db.close();
 });
 
 var app = express();
