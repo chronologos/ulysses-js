997d0bed675cc53eb989621176ae838650679457
yt61@duke.edu <yt61@duke.edu>
2016-07-11 23:44:49 -0700

    post

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'production') {
 var app = express();
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+    extended: true
 })); 
 //app.use(express.json());       // to support JSON-encoded bodies
 //app.use(express.urlencoded()); // to support URL-encoded bodies
@@ -20,21 +20,23 @@ app.get('/', function (req, res) {
 });
 
 app.post('/contract', function (req, res) {
-    console.log("contract")
+    console.log("contract");
     console.log(req.body.id);
     res.json({ userId: req.body.id});
+    MongoClient.connect(url, function(err, db) {
+        console.log("Connected correctly to server.");
+        assert.equal(null, err);
+        data = { userId: req.body.id}
+        var contracts_db = db.collection('uly-dev');
+        contracts_db.insert(data, function(err,result) {
+            if(err) throw err;
+            console.log(result);
+        });
+        db.close()
+    });
+
 });
 
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server.");
-    var contracts_db = db.collection('uly-dev');
-    //contracts_db.insert(data, function(err,result) {
-    //if(err) throw err;
-    //console.log(result);
-    //});
-    db.close()
-});
 
 app.listen(port, function () {
     console.log('Example app listening on port 3000!');
