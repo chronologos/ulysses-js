167e6efee4f1836486bc50d456f0b989c76e754e
yt61@duke.edu <yt61@duke.edu>
2016-07-11 23:36:44 -0700

    post

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
