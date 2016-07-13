44c921bf02f7e32ce778aafbbabec47f64c6bf94
yt61@duke.edu <yt61@duke.edu>
2016-07-13 07:45:10 -0700

    vue.js

--- index.js
+++ index.js
@@ -39,26 +39,27 @@ app.get('/contract/:promiserId/:promisedId/:contract/:value/:expiry', function(r
             db.close()
         });
     });
+});
 
 
-    app.post('/submit_contract', urlencodedParser, function (req, res) {
-        console.log("contract");
-        console.log(req.body.id);
-        res.json({ userId: req.body.id});
-        MongoClient.connect(url, function(err, db) {
-            console.log("Connected correctly to server.");
-            assert.equal(null, err);
-            data = { promiserId: req.body.promiserId, promisedId: req.body.promisedId, contract: req.body.contract, value: req.body.value, expiry: req.body.expiry }
-            var contracts_db = db.collection('uly-dev');
-            contracts_db.insert(data, function(err,result) {
-                if(err) throw err;
-                console.log(result);
-            });
-            db.close()
+app.post('/submit_contract', urlencodedParser, function (req, res) {
+    console.log("contract");
+    console.log(req.body.id);
+    res.json({ userId: req.body.id});
+    MongoClient.connect(url, function(err, db) {
+        console.log("Connected correctly to server.");
+        assert.equal(null, err);
+        data = { promiserId: req.body.promiserId, promisedId: req.body.promisedId, contract: req.body.contract, value: req.body.value, expiry: req.body.expiry }
+        var contracts_db = db.collection('uly-dev');
+        contracts_db.insert(data, function(err,result) {
+            if(err) throw err;
+            console.log(result);
         });
+        db.close()
     });
+});
 
 
-    app.listen(port, function () {
-        console.log('Example app listening on port 3000!');
-    });
+app.listen(port, function () {
+    console.log('Example app listening on port 3000!');
+});
