06dcbf720fe6170144898f5d9df9d7397c9106a5
adithya <adithya@yahoo-inc.com>
2016-07-17 14:10:25 -0700

    Basic database ops for saving and retrieving user contracts, mock routing

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var swig = require('swig');
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
+var mongoDB = require('mongodb');
+var MongoClient = mongoDB.MongoClient;
 var assert = require('assert');
+
+var ObjectId = mongoDB.ObjectID;
 
 var port = process.env.PORT || 3000;
 var url;
 if (process.env.NODE_ENV === 'production') {
   url = process.env.MONGODB_URI;
 } else {
-  url = 'mongodb://heroku_t776fjt0:q7iffsa51r7hd5lbev3ukmg021@ds027308.mlab.com:27308/heroku_t776fjt0';
+  //url = 'mongodb://heroku_t776fjt0:q7iffsa51r7hd5lbev3ukmg021@ds027308.mlab.com:27308/heroku_t776fjt0';
+  url = 'mongodb://127.0.0.1:27017';
 }
+
+//var contractsDB;
 
 swig.setDefaults({
   varControls: ['[[', ']]']
@@ -31,6 +37,7 @@ app.get('/', function(req, res) {
   res.render('index.html', {
     pagename: 'awesome people',
     authors: ['Paul', 'Jim', 'Jane']
+    
   });
 });
 
@@ -41,7 +48,7 @@ app.get('/contract/:contract', function(req, res) {
       // data = { promiserId: req.body.promiserId, promisedId: req.body.promisedId, contract: req.body.contract, value: req.body.value, expiry: req.body.expiry }
     var data = {contract: req.params.contract};
     console.log(data);
-    var ContractsDb = db.collection('uly-dev');
+    var ContractsDb = db.collection('contracts');
     ContractsDb.find(data).toArray(function(err, result) {
       if (err) throw err;
       if (result) {
@@ -67,6 +74,7 @@ app.post('/submit_contract', urlencodedParser, function(req, res) {
   MongoClient.connect(url, function(err, db) {
     console.log("Connected correctly to server.");
     assert.equal(null, err);
+    /*
     data = {
       promiserId: req.body.promiserId,
       promisedId: req.body.promisedId,
@@ -74,16 +82,123 @@ app.post('/submit_contract', urlencodedParser, function(req, res) {
       value: req.body.value,
       expiry: req.body.expiry
     };
-    var ContractsDb = db.collection('uly-dev');
+    */
+    
+    //var contractsDB = db.collection('uly-dev');
+    /*
     ContractsDb.insert(data, function(err, result) {
       if (err) throw err;
       console.log(result);
     });
     db.close();
-    res.status(200).end();
+    */
+    
+    // TEMP!
+    var user = req.body.promiserId;
+    //var usersDB = db.collection('users');
+    saveContractToUser(db, data, user);
   });
+  res.status(200).end();
+});
+
+// TEMP - Replace with authentication module
+app.get('/:user', function(req, res) {
+    MongoClient.connect(url, function(err, db) {
+	if (err) {
+	    res.status(501).end('Please try again in a short while');
+	    console.log("Error connecting to MongoDB");
+	    throw err;
+	}
+	retrieveUserContracts(db, req.params.user, function(error, result) {
+	    if (error) throw error;
+	    //console.log(result);
+	    //res.send(result);
+	    console.log("Passed middleware");
+	    //result.toArray().then(function(docs) {
+		//console.log("First document");
+		//if (docs) console.log(docs[0]);
+	    //});
+	    if (result) console.log("Contract objects are ");
+	    //console.log(result['contracts']);
+	    //console.log(JSON.stringify(result));
+	    console.log(result);
+	    //db.close();
+	    res.json(result);
+	    db.close();
+	});
+    });
+
 });
 
 app.listen(port, function() {
   console.log('Example app listening on port 3000!');
 });
+
+
+
+/*
+* Save contract object to DB
+* Retrieve _id from DB
+* Push it onto user's contracts list
+*/
+function saveContractToUser(db, contract, user) {
+    //contractsDB.insert(contract)
+    var contractsDB = db.collection('contracts');
+    var usersDB = db.collection('users');
+    var contractID;
+    contractsDB.insert(contract, function(err, result) {
+    if (err) throw err;
+	console.log(result);
+	contractID = result['insertedIds'][0];
+	console.log("ContractID is " + contractID);
+	usersDB.update({'userName':user}, {'$push':{'contracts':contractID}}, {'upsert' : true}, function(error, res) {
+	    if (error) throw error;
+	    console.log("Successfully appended " + contractID + " to user " + user);
+	    console.log(res);
+	    db.close();
+	});
+    });
+} 
+
+
+function retrieveUserContracts(db, userName, next) {
+    var usersDB = db.collection('users');
+    var contractsDB = db.collection('contracts');
+    // Try and replace with more efficient query that only retrieves the contracts field
+    usersDB.find({'userName':userName}, function(error , result) {
+	if (!error) {
+	    result.limit(1).toArray().then(function(docs, err) { // Assumed that user identifier will be unique, but limit 1 just in case
+		if (err) next(err, docs);
+		console.log("Passing matching document :");
+		console.log(docs[0]);
+		getContracts(contractsDB, docs[0]['contracts'], next);
+	    });
+	}
+	else {
+	    next(error, result);
+	}
+    });
+
+}
+
+
+function getContracts(contractsDB, idsList, next) {
+    var contractObjs = [];
+    console.log('IDs list has length ' + idsList.length);
+    idsList.forEach(function(contractID, index) {
+	console.log("Getting contracts with list");
+	contractsDB.find(ObjectId(contractID)).toArray().then(function(docs, err) {
+	    if (err) {
+		console.log("Alert! Failed to fetch contract no. " + (index + 1));
+		//continue;
+		return;
+	    };
+	    console.log("Retrieved document for " + contractID);
+	    console.log(docs);
+	    contractObjs.push(docs[0]);
+	    if (index == idsList.length - 1) {
+		next(null, contractObjs);
+	    }
+	});
+    });
+}
